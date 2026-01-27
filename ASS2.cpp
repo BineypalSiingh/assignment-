a2492f7f396ad15efa1a0c9ee0ece7945b0b099e
Bineypal SINGH <bsingh6_be24@thapar.edu>
2026-01-27 15:25:19 +0530

    Add files via upload

---
+++ ASS2.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+//1
+//class aim{
+    //     float target;
+    //     public:
+    //     void game();
+        
+    // };
+    // void aim:: game(){
+    //         float target;
+    //             cout<<"enter target distance"<<endl;
+    //             cin>>target;}
+            
+    // int main(){
+    //     aim a;
+    //     a.game();
+    //     return 0;
+
+    // }
+
+
+    //4
+//     class rectangle {
+//         float length;
+//         float breadth;
+//         public:
+//         void getdata();
+//         void calculatearea();
+
+//     };
+    
+// void rectangle::getdata(){
+//     cin>>length;
+//     cin>>breadth;
+    
+// }
+// void rectangle::calculatearea(){
+//     cout<<length*breadth;
+
+// }
+// int main(){
+// rectangle r;
+// r.getdata();
+// r.calculatearea();
+
+    // //3
+
+
+    // class aim{
+    //     float target;
+    //     public:
+    //     void game();
+        
+    // };
+    // void aim:: game(){
+    //         float target;
+    //             cout<<"enter target distance"<<endl;
+    //             cin>>target;}
+            
+    // int main(){
+    //     aim a;
+    //     a.game();
+    //     return 0;
+
+    // }
+  
+
+
+    //5
+//     class number{
+//         float real;
+//         float imaginary;
+//         public:
+
+       
+        
+//         void sum(){
+//             cout<<"enter real part"<<endl;
+//             cin>>real;
+//             cout<<"enter imaginary part"<<endl;
+//             char i=i;
+//             cin>>imaginary;
+//            // cout<<real<<"+"<<imaginary<<"i";
+//         }
+//    void complex(){
+//             cout<<real<<"+"<<imaginary<<"i"; }};
+//     int main(){
+// number c;
+// c.sum();
+// cout<<"calling complex function "<<endl;
+// c.complex();
+// return 0;
+   // }
+
+    //6
+    class rectangle {
+        float length;
+        float breadth;
+        public:
+        void getdata();
+        void calculatearea();
+
+    };
+    
+void rectangle::getdata(){
+    cin>>length;
+    cin>>breadth;
+}
+int main(){
+    rectangle.r;
+    return 0;
+    
+}
